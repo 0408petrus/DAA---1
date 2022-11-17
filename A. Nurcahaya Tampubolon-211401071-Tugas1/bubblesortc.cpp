e26de1347d33e62f7d4670ac51434933cec4060a
clvdyo <anurcahayatampubolon@gmail.com>
2022-11-17 18:53:16 +0700

    update tugas1 cahaya

--- A. Nurcahaya Tampubolon-211401071-Tugas1/bubblesortc.cpp
+++ A. Nurcahaya Tampubolon-211401071-Tugas1/bubblesortc.cpp
@@ -1,16 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout<<"Bubble Sort\n";
-    int data[8]={10,11,23,21,0,0,8,7};
-    for (int i=1;i<8;i++){
-        for(int j=7;j>=i;j--){
-            if (data[j]>data[j-1])
-            swap(data[j],data[j-1]);
-        }
-    }
-    for (int i=0;i<8;i++){
-        cout<<data[i]<<" ";
-    }
+int main()
+{
+    cout<<"Bubble Sort\n\n";
 
+    int n=5;
+    int data[5];
+    cout<<"Input data : \n\n";
+    for (int i=0;i<5;i++)
+    {
+        cout<<"Data-"<<i+1<<":";
+        cin>>data[i];
+    }
+    //----bubble sort
+    for (int i=0;i<n;i++)
+    {
+        for (int j=n-1;j>=i;j--)
+    {
+        if(data[j]>data[j-1]) //descending
+            swap(data[j],data[j-1]);
+    }
+    cout<<data[i]<<" ";
+    }
 }
